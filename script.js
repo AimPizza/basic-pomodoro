437c462d933627144207a3458d7d912d4930f09c
AimPizza <rickomo.us@gmail.com>
2024-12-16 16:25:43 +0100

    feat: complete state management with localStorage and add UI improvements

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-// TODO: time is very low for testing purposes
-const STUDY_DURATION = 7; // time in seconds
+//const STUDY_DURATION = 25 * 60; // time in seconds
+//const BREAK_DURATION = 5 * 60; // time in seconds
+const STUDY_DURATION = 5; // time in seconds
 const BREAK_DURATION = 3; // time in seconds
 
-// const State = {
-//     STUDYING,
-//     PAUSING,
-//     STOPPED,
-// };
-
 const pomodoroStatus = {
-  STUDYING: 'STUDYING',
-  STUDY_PAUSED: 'STUDY_PAUSED',
-  ON_BREAK: 'ON_BREAK',
-  BREAK_PAUSED: 'BREAK_PAUSED',
+  STUDYING: 'studying',
+  STUDY_PAUSED: 'study paused',
+  ON_BREAK: 'on break',
+  BREAK_PAUSED: 'break paused',
 };
 
 class TimerState {
@@ -21,7 +16,6 @@ class TimerState {
   timesStudied;
 
   constructor() {
-    this.currentStatus = pomodoroStatus.STUDY_PAUSED;
     this.timesOnBreak = 0;
     this.timesStudied = 0;
   }
@@ -30,44 +24,123 @@ class TimerState {
     this.timesOnBreak += 1;
   }
 
-  // getSavedStatus() {};
+  incrementTimesStudied() {
+    this.timesStudied += 1;
+  }
+
+}
+
+class StorageManager {
+  constructor(key) {
+    this.key = key;
+  }
+
+  save(data) {
+    localStorage.setItem(this.key, JSON.stringify(data));
+  }
+
+  load() {
+    const data = localStorage.getItem(this.key);
+    return data ? JSON.parse(data) : null;
+  }
+
+  clear() {
+    localStorage.removeItem(this.key);
+  }
 }
 
 class Timer {
   tTime = document.querySelector('#timer-time');
   tResetButton = document.querySelector('#button-reset');
   tToggleButton = document.querySelector('#button-toggle');
+  tStatusText = document.querySelector('#timer-statustext');
   timerId;
-  timeLeft = STUDY_DURATION;
+  timeLeft;
   seconds;
   minutes;
   timerState = new TimerState();
+  storageManger = new StorageManager('timerData');
 
   constructor() {
+    this.displayTime = this.showStatus.bind(this);
+    this.setStatus = this.setStatus.bind(this);
+    this.setTimeLeft = this.setTimeLeft.bind(this);
     this.runTimer = this.runTimer.bind(this);
-    this.displayTime = this.displayTime.bind(this);
-    this.finishState = this.finishState.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
     this.toggleState = this.toggleState.bind(this);
-    this.tToggleButton.addEventListener('click', this.toggleState)
+    this.finishState = this.finishState.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.tToggleButton.addEventListener('click', this.toggleState);
+    this.tResetButton.addEventListener('click', this.resetTimer);
 
-    this.displayTime();
-    console.log("constructed");
+
+    this.loadStateOrDefault();
+    window.addEventListener('beforeunload', () => {this.saveState();});
+    console.log("constructed, waiting for input..");
+  }
+
+  saveState() {
+    const data = {
+      currentStatus: this.timerState.currentStatus,
+      timeLeft: this.timeLeft,
+      timesOnBreak: this.timerState.timesOnBreak,
+      timesStudied: this.timerState.timesStudied
+    };
+    this.storageManger.save(data);
+  }
+
+  // load settings and state from localStorage (as of now)
+  // if null -> set defaults
+  loadStateOrDefault() {
+    const data = this.storageManger.load();
+    if (data) {
+      switch (data.currentStatus) {
+        case pomodoroStatus.STUDYING:
+          this.setStatus(pomodoroStatus.STUDY_PAUSED);
+          break;
+        case pomodoroStatus.ON_BREAK:
+          this.setStatus(pomodoroStatus.BREAK_PAUSED);
+          break;
+        default:
+          this.setStatus(data.currentStatus);
+      }
+      this.setTimeLeft(data.timeLeft);
+      this.timerState.timesOnBreak = data.timesOnBreak;
+      this.timerState.timesStudied = data.timesStudied;
+    } else {
+      this.setStatus(pomodoroStatus.STUDY_PAUSED);
+      this.setTimeLeft(STUDY_DURATION);
+      this.timerState.timesOnBreak = 0;
+      this.timerState.timesStudied = 0;
+    }
+    this.showStatus();
   }
 
   formatNumbers(num) {
     return String(num).padStart(2, '0')
   }
-  displayTime() {
+
+  showStatus() {
     this.seconds = this.timeLeft % 60;
     this.minutes = (this.timeLeft - this.seconds) / 60;
     this.tTime.textContent = `${this.formatNumbers(this.minutes)}:${this.formatNumbers(this.seconds)}`;
+    this.tStatusText.textContent = this.timerState.currentStatus;
   }
+
   stopTimer() {
     clearInterval(this.timerId);
   }
+
   setStatus(newStatus) {
     this.timerState.currentStatus = newStatus;
+    this.showStatus();
   }
+
+  setTimeLeft(secondsLeft) {
+    this.timeLeft = secondsLeft;
+    this.showStatus();
+  }
+
   toggleState() {
     // note that we stop timers at the very beginning and start timers at the end
     switch (this.timerState.currentStatus) {
@@ -90,37 +163,48 @@ class Timer {
       default:
         break;
     }
+    this.saveState();
   }
+
   finishState() {
     switch (this.timerState.currentStatus) {
       case pomodoroStatus.STUDYING:
         console.log('finished studying. now on break..');
         this.stopTimer();
-        this.timerState.currentStatus = pomodoroStatus.ON_BREAK;
-        this.timeLeft = BREAK_DURATION;
-        this.displayTime();
+        this.setStatus(pomodoroStatus.ON_BREAK)
+        this.setTimeLeft(BREAK_DURATION);
         this.runTimer();
+        this.timerState.incrementTimesStudied();
         break;
       case pomodoroStatus.ON_BREAK:
         console.log('break finished. now studying..');
         this.stopTimer();
-        this.timerState.currentStatus = pomodoroStatus.STUDYING;
-        this.timeLeft = STUDY_DURATION;
-        this.displayTime();
+        this.setStatus(pomodoroStatus.STUDYING)
+        this.setTimeLeft(STUDY_DURATION);
         this.runTimer();
+        this.timerState.incrementTimesOnBreak();
         break;
       default:
-        console.log('Unhandled state was finished.');
+        console.error('Unhandled state was finished.');
         break;
     }
+    this.saveState();
   };
+
   runTimer() {
     this.timerId = setInterval(() => {
       this.timeLeft--;
-      this.displayTime();
+      this.showStatus();
       if (this.timeLeft <= 0) { this.finishState(); }
     }, 1000);
   }
+
+  resetTimer() {
+    this.stopTimer();
+    this.setTimeLeft(STUDY_DURATION);
+    this.setStatus(pomodoroStatus.STUDY_PAUSED);
+  }
+
 };
 
 let timer = new Timer();
